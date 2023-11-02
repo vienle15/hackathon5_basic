29d6b204a4b229816e0607e01a7c343365e26037
vienle15 <tanvien93lt@gmail.com>
2023-11-02 11:30:55 +0700

    basic

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,24 +1,26 @@
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
+
 class UsersController {
   getAllUsers(req, res) {
     const fileUsers = fs.readFileSync("./src/models/users.json", "utf8");
     const listUsers = JSON.parse(fileUsers);
-
     res.json(listUsers);
   }
+
   getUsersById(req, res) {
     const id = req.params.id;
     const fileUsers = fs.readFileSync("./src/models/users.json", "utf8");
     const listUsers = JSON.parse(fileUsers);
     const user = listUsers.find((item) => item.id == id);
+
     if (user) {
       res.json(user);
     } else {
       res.status(404).json({ error: "User not found" });
     }
-    res.json(user);
   }
+
   createUser(req, res) {
     const newUser = req.body;
     newUser.id = uuidv4();
@@ -27,21 +29,21 @@ class UsersController {
     const listUsers = JSON.parse(fileUsers);
     listUsers.push(newUser);
     fs.writeFileSync("./src/models/users.json", JSON.stringify(listUsers));
-    res.json({
-      status: 201, // Trạng thái 201 (Created) cho thành công khi tạo người dùng mới
+    res.status(201).json({
       message: "User created successfully",
       data: newUser,
     });
   }
+
   deleteUser(req, res) {
     const id = req.params.id;
     const fileUsers = fs.readFileSync("./src/models/users.json", "utf8");
     const listUsers = JSON.parse(fileUsers);
     const newListUser = listUsers.filter((item) => item.id != id);
     fs.writeFileSync("./src/models/users.json", JSON.stringify(newListUser));
-
     res.json(newListUser);
   }
+
   updateUser(req, res) {
     const id = req.params.id;
     const fileUsers = fs.readFileSync("./src/models/users.json", "utf8");
@@ -55,9 +57,10 @@ class UsersController {
     fs.writeFileSync("./src/models/users.json", JSON.stringify(listUsers));
     res.json({
       status: 200,
-      message: "Ok",
+      message: "User updated successfully",
       data: listUsers,
     });
   }
 }
+
 export default UsersController;
